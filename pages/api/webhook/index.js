f986369f022d274d5f954386229039b1cb94a566
Ayush Maurya <ayushmaurya.work@gmail.com>
2021-08-29 19:38:29 +0530

    webhook fix

--- pages/api/webhook/index.js
+++ pages/api/webhook/index.js
@@ -16,7 +16,7 @@ export default async function handler(req, res) {
       event = stripe.webhooks.constructEvent(
         rawBody.toString(),
         signature,
-        process.env.STRIPE_WEBHOOK_KEY
+        process.env.STRIPE_WEBHOOK_KEY.toString()
       );
     } catch (err) {
       res.status(400).send(`Webhook error ${err.message}`);
