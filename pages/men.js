42e4ecab00d99c7a41462aaeacf20881c7b5d334
Ayush Maurya <ayushmaurya.work@gmail.com>
2021-08-30 21:03:19 +0530

    fixes

--- pages/men.js
+++ pages/men.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import { GlobalContext } from 'context/GlobalState';
+
 import { useContext } from 'react';
 const men = () => {
-  const { cartItems, addItems } = useContext(GlobalContext);
-
   return <div>this is the men's store</div>;
 };
 
