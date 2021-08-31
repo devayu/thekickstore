1fd6548d9029bcfce56092b68183841261299e61
Ayush Maurya <ayushmaurya.work@gmail.com>
2021-08-31 17:53:38 +0530

    [fix]:navigation

--- components/Header.js
+++ components/Header.js
@@ -37,7 +37,7 @@ const Header = () => {
               </Link>
             </li>
             <li>
-              <Link href='/success'>
+              <Link href='/brands'>
                 <a>Brands</a>
               </Link>
             </li>
